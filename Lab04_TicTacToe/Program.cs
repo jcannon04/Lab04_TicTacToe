9819566e23b434bbf35379b1effa6365d7d22039
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-07-13 18:43:39 -0500

    init commit

---
+++ Lab04_TicTacToe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Board = Lab04_TicTacToe.Classes.Board;
+
+namespace Lab04_TicTacToe
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+            Board board = new();
+            board.DisplayBoard();
+            Console.ReadLine();
+        }
+
+
+    }
+}
